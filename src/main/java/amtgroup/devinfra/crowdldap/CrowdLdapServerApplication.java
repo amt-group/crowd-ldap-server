0589e6ad034b17fa99998148c44ee813ebc9456a
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2018-06-09 11:07:48 +0300

    feat: Project created with Spring Initializr.

---
+++ src/main/java/amtgroup/devinfra/crowdldap/CrowdLdapServerApplication.java
@@ -0,0 +1,12 @@
+package amtgroup.devinfra.crowdldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdLdapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdLdapServerApplication.class, args);
+    }
+}
